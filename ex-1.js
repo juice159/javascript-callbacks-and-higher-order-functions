ad6ced63021bea260b754cba471e71de4b4bcd36
Thanutham Baikwang <thanutham.j@gmail.com>
2024-09-06 11:45:34 +0700

    feat: add answer to excercise

--- ex-1.js
+++ ex-1.js
@@ -1,12 +1,17 @@
 // Exercise #1: For Each Function
 
 function forEach(array, operation) {
-  // Start coding here
+  const result = [];
+  for (let i = 0 ; i < array.lenght; i++) {
+    operation(array[i]);
+  }
 }
 
 const employeeSalaries = [20005, 40000, 32000, 14500, 344000];
 const newEmployeeSalaries = [];
 
-// Using `forEach` function here
-
-console.log(newEmployeeSalaries); // [25005, 45000, 37000, 19500, 349000]
+function add5000(oldSalary) {
+  newEmployeeSalaries.push(oldSalary + 5000); 
+}
+forEach(employeeSalaries, add5000);
+console.log(newEmployeeSalaries); 
